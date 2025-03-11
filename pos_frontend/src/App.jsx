c60f13b55fa49d859fedad57fe0c7cfa29f0589f
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-11 23:38:54 +0530

    bit changes

--- pos_frontend/src/App.jsx
+++ pos_frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Auth, Orders } from "./pages";
 
 const App = () => {
   return (

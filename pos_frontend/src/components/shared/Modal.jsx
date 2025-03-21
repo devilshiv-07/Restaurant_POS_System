db0bdc52f5e58c8174b64fa9ded1b67fba819c40
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-22 00:40:15 +0530

    Modal component is done

---
+++ pos_frontend/src/components/shared/Modal.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Modal = ({title, onClose, isOpen, children}) => {
+
+    if (!isOpen) return null;
+
+  return (
+    // set background blur
+    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
+
+      {/* main div */}
+      <div className='bg-[#1a1a1a] shadow-lg w-full max-w-lg mx-4 rounded-lg py-2 px-4'>
+
+        {/* Header */}
+        <div className='flex justify-between items-center px-6 py-2 border-b border-[#333]'>
+            {/* title */}
+            <h2 className='text-xl text-[#f5f5f5] font-semibold'>{title}</h2>
+
+            {/* close button */}
+            <button className='text-gray-500 text-2xl hover:text-gray-300' onClick={onClose}>
+                &times;
+            </button>
+        </div>
+
+        {/* Content */}
+        <div className='p-4'>
+            {children}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Modal

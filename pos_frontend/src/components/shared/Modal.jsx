0acd6606788e742a28fda366d4c9bcd4fc900381
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-22 07:58:21 +0530

    Bit Changes in Modal Component

--- pos_frontend/src/components/shared/Modal.jsx
+++ pos_frontend/src/components/shared/Modal.jsx
@@ -9,12 +9,12 @@ const Modal = ({title, onClose, isOpen, children}) => {
     <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
 
       {/* main div */}
-      <div className='bg-[#1a1a1a] shadow-lg w-full max-w-lg mx-4 rounded-lg py-2 px-4'>
+      <div className='bg-[#1a1a1a] shadow-lg max-w-lg mx-4 rounded-lg py-2 px-4 w-[30vw]'>
 
         {/* Header */}
-        <div className='flex justify-between items-center px-6 py-2 border-b border-[#333]'>
+        <div className='flex justify-between items-center px-6 py-4 pb-6 border-b border-[#333]'>
             {/* title */}
-            <h2 className='text-xl text-[#f5f5f5] font-semibold'>{title}</h2>
+            <h2 className='text-2xl text-[#f5f5f5] font-semibold'>{title}</h2>
 
             {/* close button */}
             <button className='text-gray-500 text-2xl hover:text-gray-300' onClick={onClose}>

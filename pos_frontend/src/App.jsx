f082a06f4f57bfea45d0e1548aa01f58dcc2bdba
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-23 20:01:28 +0530

    NotFound Page is done

--- pos_frontend/src/App.jsx
+++ pos_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Auth, Orders, Tables } from "./pages";
+import { Home, Auth, Orders, Tables, Menu, NotFound } from "./pages";
 import Header from './components/shared/Header';
 
 const App = () => {
@@ -13,6 +13,8 @@ const App = () => {
         <Route path='/auth' element={<Auth />} />
         <Route path='/orders' element={<Orders />} />
         <Route path='/tables' element={<Tables />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
     </>
---
+++ pos_frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='h-[101vh] font-extrabold text-6xl flex items-center justify-center mt-[-10vh] bg-[#23253A]'>
+        <div className='w-[45vw] text-[#d64c29]'>
+        😌 Sorry This Page is Not Found
+        </div>
+    </div>
+  )
+}
+
+export default NotFound

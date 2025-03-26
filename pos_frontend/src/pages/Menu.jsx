26ca18af46f5e98836b1c492279c1fe3d3a03a8d
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-26 23:49:09 +0530

    Update Menu.jsx

--- pos_frontend/src/pages/Menu.jsx
+++ pos_frontend/src/pages/Menu.jsx
@@ -1,31 +1,41 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
 import BackButton from "../components/shared/BackButton";
+import { FaUserCircle } from "react-icons/fa";
 
 const Menu = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
 
       {/* Left div */}
-      {/* <div className="flex-[3] bg-red-500"></div>
+      <div className="flex-[3]">
+        <div className="flex items-center justify-between px-10 py-2 mt-2">
 
-      <div className="flex items-center justify-between px-10 py-2 mt-2">
-
-        Menu Heading
-        <div className="flex items-center gap-4">
-          <BackButton />
-          <h1 className="text-[#f5f5f5] text-xl font-bold tracking-wide">
-            Menu
-          </h1>
+          {/* Menu Heading */}
+          <div className="flex items-center gap-4">
+            <BackButton />
+            <h1 className="text-[#f5f5f5] text-xl font-bold tracking-wide">
+              Menu
+            </h1>
+          </div>
+          
+          {/* Admin info and table */}
+          <div className="flex items-center justify-around gap-4">
+            <div className="flex items-center gap-3 cursor-pointer">
+              <FaUserCircle className="text-[#f5f5f5] text-2xl " />
+              <div className="flex flex-col items-start">
+                <h1 className="text-sm text-[#f5f5f5]  font-semibold">
+                  Cumstomer Name
+                </h1>
+                <p className="text-xs text-[#ababab] font-medium">Admin</p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="flex items-center justify-around gap-4">
-          ALL
-          <button>
-            All
-          </button>
-        </div>
-      </div> */}
+      {/* Right div */}
+      <div className="flex-[1] bg-blue-500"></div>
 
       <BottomNav />
     </section>

f08163566090d694ec218251ec17d47821e122ac
Shivank Tripathi <182352677+devilshiv-07@users.noreply.github.com>
2025-03-29 22:29:55 +0530

    Update Menu.jsx

--- pos_frontend/src/pages/Menu.jsx
+++ pos_frontend/src/pages/Menu.jsx
@@ -7,13 +7,10 @@ import MenuContainer from "../components/menu/MenuContainer";
 const Menu = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
-
       {/* Left div */}
       <div className="flex-[3]">
-
         {/* Header of Left div */}
         <div className="flex items-center justify-between px-10 py-2 mt-2">
-
           {/* Menu Heading */}
           <div className="flex items-center gap-4">
             <BackButton />
@@ -21,7 +18,7 @@ const Menu = () => {
               Menu
             </h1>
           </div>
-          
+
           {/* Admin info and selected table */}
           <div className="flex items-center justify-around gap-4">
             <div className="flex items-center gap-3 cursor-pointer">
@@ -30,7 +27,9 @@ const Menu = () => {
                 <h1 className="text-sm text-[#f5f5f5]  font-semibold">
                   Cumstomer Name
                 </h1>
-                <p className="text-xs text-[#ababab] font-medium">Table No: 12</p>
+                <p className="text-xs text-[#ababab] font-medium">
+                  Table No: 12
+                </p>
               </div>
             </div>
           </div>
@@ -40,7 +39,40 @@ const Menu = () => {
       </div>
 
       {/* Right div */}
-      <div className="flex-[1] bg-blue-500"></div>
+      <div className="flex-[1] bg-[#1a1a1a] mt-4 mr-3 h-[78vh] rounded-lg pt-2">
+
+        {/* Customer Info */}
+        <div className="flex items-center justify-between px-4 py-2 h-[15%]">
+
+          {/* Customer name, table and date */}
+          <div className="flex flex-col items-start">
+            <h1 className="text-sm text-[#f5f5f5] font-semibold tracking-wide">
+              Customer Name
+            </h1>
+            <p className="text-xs text-[#ababab] font-medium mt-1">
+              #101/Dine in
+            </p>
+            <p className="text-xs text-[#ababab] font-medium mt-2">
+              January 19, 2025 05:34 PM
+            </p>
+          </div>
+          {/* Customer name as logo */}
+          <button className="bg-[#f6b100] p-2 text-lg font-bold rounded-lg">
+            CN
+          </button>
+        </div>
+
+        {/* Separator line */}
+        <hr className="border-[#2a2a2a] border-t-2 h-[2%]" />
+
+        {/* Cart Item */}
+        <div className="px-4 py- h-[71%]">
+          <h1 className="text-lg text-[#e4e4e4] font-semibold tracking-wide">Order Details</h1>
+          <div className="mt-2 overflow-y-scroll scrollHide h-[85%]">
+            <h1 className="text-[#ababab] font-semibold tracking-wide text-sm">Dal Makhani</h1>
+          </div>
+        </div>
+      </div>
 
       <BottomNav />
     </section>
